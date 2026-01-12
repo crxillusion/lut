3ab5f35818fd7ca063ca4d1de0c32516cc469751
Kim <crxillusion@gmail.com>
2026-01-12 20:53:30 +0400

    Fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/lut',
   images: {
     unoptimized: true,
   },
-  // Set the base path if your repo name is not 'lut'
-  // basePath: '/your-repo-name',
 };
 
 export default nextConfig;
