d86ee737aeb4064f5e43260adad59f029f22ae50
Kim <crxillusion@gmail.com>
2026-02-10 15:30:33 +0400

    refactor: Update basePath configuration for production environment

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/lut' : '',
+  basePath: isProd ? '' : '',
   images: {
     unoptimized: true,
   },
